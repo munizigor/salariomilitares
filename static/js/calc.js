a8a3f7ea28b5f2ebe1fd6b95df2283a8aa352a30
¨Igor <muniz.igor@gmail.com>
2020-03-23 19:37:54 -0300

    Iniciando calculo de verbas

--- static/js/calc.js
+++ static/js/calc.js
@@ -1,10 +1,75 @@
-function calcular_rubricas (rubrica){
-
-    function getMes (coluna) {
-        return document.getElementById('mes').cells[coluna].headers
-    }
-
+function getMes (coluna) {
+    return document.getElementById('mes').cells[coluna].headers
 }
+function valor_parcela (rubrica) {
+    if (!isNaN(remuneracao[rubrica])) {
+        return remuneracao[rubrica]
+    }
+    else {
+        return remuneracao[document.getElementById('posto').value][rubrica]
+    }
+}
+
+function parseMoney (x) {
+    return 'R$ '+ x.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})
+}
+
+
+function calcular_rubricas (){
+
+    for (col=1;col<document.getElementById('mes').cells.length;col++){
+        mes = getMes(col)
+        salario_row = document.getElementById('salario').rows
+        for (row=0;row<salario_row.length;row++){
+            if (salario_row[row].hasAttribute('id')){
+                rubrica = valor_parcela (salario_row[row].id)
+                celula = salario_row[row].cells[col]
+                if (typeof(rubrica)=='object' && !isNaN(rubrica[mes])) {
+                    celula.innerHTML = parseMoney (rubrica[mes])
+                }
+                else if (!isNaN(rubrica)) {
+                    celula.innerHTML = parseMoney (rubrica)
+                }
+                else {
+                    celula.innerHTML = 'NÃO ENCONTRADO' //TODO Manter por enquanto para identificar erros
+                }
+                //TODO:Calcular complemento de Soldo
+                //TODO:Chamar outras funções de parcelas específicas
+            }
+            else {
+                continue
+            }
+        }
+    }
+}
+
+function calc_ats() {
+}
+
+function calc_acp() {
+}
+
+function calc_gfne() {
+}
+
+function calc_pttc() {
+}
+
+function calc_gsv() {
+}
+
+function calc_aux_mor() {
+}
+
+function calc_pre_esc() {
+//TODO: Calcular Pré-Escolar e Cota
+}
+
+function calc_pensao_alim() {
+//TODO: Calcular Pensao Alimentícia e Pensao sobre pré escolar
+}
+
+
 /*
 //-Alterar parametro
 //-Aciona funcao de calculo de rubricas
